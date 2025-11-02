64ef702352bc9e9ef11a45adadca0ef34fd449bc
ylimas <yagovianaalencar23@gmail.com>
2025-11-02 11:39:31 -0300

    Add files via upload

---
+++ tadpilha.py
@@ -0,0 +1,13 @@
+def add_pilha(elem):
+
+	pilha = {"top":"", "mid":[], "bottom":""}
+		if pilha["top"] == "":
+			if pilha["mid"] == []:
+				pilha["bottom"] = top
+
+			pilha["top"] = str(elem)
+			pilha["bottom"] = str(elem)
+		
+		elif pilha["mid"] == []:
+			pilha["mid"].append(elem)
+			
